aebdce86fe9a58e55ce18d203892d4d533952d66
Dario Mauri <dario.mauri_ext@zooplus.com>
2022-08-19 18:19:39 +0200

    Remove vertical scrolling of dog picture in dog profile
    - still needs improving, ideally to fill the space horizontally without causing vertical scrolling
    - remove publishing package to github

--- src/main/java/com/dario/iftv/view/DogProfileDialog.java
+++ src/main/java/com/dario/iftv/view/DogProfileDialog.java
@@ -1,7 +1,6 @@
 package com.dario.iftv.view;
 
 import com.dario.iftv.core.domain.Dog;
-import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H3;
@@ -12,6 +11,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import static com.dario.iftv.util.Utils.createIconByGender;
 import static com.dario.iftv.util.Utils.formatBirthDate;
+import static com.vaadin.flow.component.Unit.PERCENTAGE;
 import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.END;
 import static java.lang.String.format;
@@ -19,8 +19,8 @@ import static java.lang.String.format;
 public class DogProfileDialog extends Dialog {
 
     public DogProfileDialog(Dog dog) {
-        setMinWidth(30, Unit.PERCENTAGE);
-        setMaxHeight(90, Unit.PERCENTAGE);
+        setMinWidth(30, PERCENTAGE);
+        setMaxHeight(90, PERCENTAGE);
 
         H3 name = new H3(dog.getName());
         name.getStyle().set("margin", "0");
@@ -36,13 +36,14 @@ public class DogProfileDialog extends Dialog {
         Image image = dog.getImageUrl().isBlank()
                 ? new Image("/images/img-not-found.jpg", "Image not available")
                 : new Image(dog.getImageUrl(), "");
-        image.setWidthFull(); // TODO avoid vertical scrolling
+        // TODO avoid vertical scrolling of image
 
         Button closeButton = new Button("Close");
         closeButton.addClickListener(event -> close());
 
         VerticalLayout layout = new VerticalLayout(nameGenderLayout, countryBirthdayColor, image, closeButton);
         layout.setHorizontalComponentAlignment(END, closeButton);
+        layout.setHorizontalComponentAlignment(CENTER, image);
         layout.setPadding(false);
 
         add(layout);
